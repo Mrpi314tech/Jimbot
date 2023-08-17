bf2a00dd5295588dff3138a5bf633443105612c2
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-08-17 14:48:54 -0400

    Create Jimbot_update.sh

---
+++ Jimbot_update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+location="$HOME"
+mv ~/Jimbot/Python/new_words.py ~
+mv ~/Jimbot/Welcome/info.py ~
+mv ~/Jimbot/Python/new_com.py ~
+mv ~/Jimbot/Python/skills ~
+echo 'type "y" if it asks you a question: '
+rm -r ~/Jimbot
+git clone https://github.com/Mrpi314tech/Jimbot
+rm ~/Jimbot/Python/new_words.py
+rm ~/Jimbot/Welcome/info.py
+rm ~/skills/install_skills.py
+mv ~/Jimbot/Python/skills/install_skills.py ~/skills
+rm -r ~/Jimbot/Python/skills
+mv ~/skills ~/Jimbot/Python
+mv ~/new_words.py ~/Jimbot/Python
+sudo pip install keyboard
+sudo apt-get install wmctrl
+mv ~/info.py ~/Jimbot/Welcome
+mv ~/new_com.py ~/Jimbot/Python
+mv ~/Jimbot_update.sh ~/delete_this_file-Jimbot
+sudo apt-get install xterm
+echo "done"
+echo "Jimbot is installed!"
+echo "The Jimbot app can be found in Menu>System Tools>Jimbot"
+chmod +x ~/Jimbot/Jimbot
+chmod +x ~/Jimbot/Jimbot_remove.sh
+chmod +x ~/Jimbot/Jimbot_install.sh
+echo 'type "y" if it asks you a question: '
+rm -r ~/Jimbot/.git
+chmod +x ~/Jimbot/Jimbot_update.sh
+mv ~/Jimbot/Jimbot_update.sh ~
