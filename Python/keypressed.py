12307d49a32f2ae45b971d9f77393ef85a3ef3e8
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-09-20 20:56:15 -0400

    switch to pynput library

--- Python/keypressed.py
+++ Python/keypressed.py
@@ -1,10 +1,14 @@
-#sudo python3 ~/Jimbot/Python/keypressed.py
-import keyboard
 import os
-file_location=os.path.expanduser('~')
-def hotkey(event):
-    if event.name == 'f10':
-        os.system("window_id=$(wmctrl -l | grep 'Jimbot' | awk '{print $1}') && wmctrl -i -a $window_id")
-keyboard.on_press(hotkey)
-keyboard.wait()
+from pynput import keyboard
 
+file_location = os.path.expanduser('~')
+
+def hotkey(key):
+    try:
+        if key == keyboard.Key.f10:
+            os.system("window_id=$(wmctrl -l | grep 'Jimbot' | awk '{print $1}') && wmctrl -i -a $window_id")
+    except AttributeError:
+        pass
+
+with keyboard.Listener(on_press=hotkey) as listener:
+    listener.join()
