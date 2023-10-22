84a791b6cd0863f8de554f31c381a5963ccb830a
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-10-22 17:23:13 -0400

    Add updates for better google search function

--- Python/Jimbot_Windows.py
+++ Python/Jimbot_Windows.py
@@ -193,6 +193,92 @@ r=sr.Recognizer()
 # Fart
 def stinky():
     os.system('xdg-open ~/Jimbot/sounds/fart.mp3')
+# Google Search
+from bs4 import BeautifulSoup
+import re
+pattern = r'([a-zA-Z])(\d)'
+pattern2 = r'([a-z])([A-Z])'
+pattern3 = r'([A-Z])([A-Z])'
+pattern4 = r'(\d)([a-zA-Z])'
+pattern5= r'([A-Z])'
+def google_search(url):
+    url=url.replace('+','plus')
+    url="https://www.google.com/search?q="+url.replace(' ','+')
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        text_elements = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span', 'div'])
+        
+        page_text = ' '.join(element.get_text() for element in text_elements)
+        page_text=page_text.replace(' Sun', ' Sunday')
+        page_text=page_text.replace(' Mon', ' Monday')
+        page_text=page_text.replace(' Tue', ' Tuesday')
+        page_text=page_text.replace(' Wed', ' Wednesday')
+        page_text=page_text.replace(' Thu', ' Thursday')
+        page_text=page_text.replace(' Fri', ' Friday')
+        page_text=page_text.replace(' Sat', ' Saturday')
+        
+        page_text=page_text.replace(' Jan', ' January')
+        page_text=page_text.replace(' Feb', ' February')
+        page_text=page_text.replace(' Mar', ' March')
+        page_text=page_text.replace(' Apr', ' April')
+        page_text=page_text.replace(' Jun', ' June')
+        page_text=page_text.replace(' Jul', ' July')
+        page_text=page_text.replace(' Aug', ' August')
+        page_text=page_text.replace(' Sep', ' September')
+        page_text=page_text.replace(' Oct', ' October')
+        page_text=page_text.replace(' Nov', ' November')
+        page_text=page_text.replace(' Dec', ' December')
+        page_text=page_text.replace('°', ' degrees ')
+        page_text=page_text.replace('\u202f', ' ')
+        page_text=page_text.replace('\u203a', ' ')
+        #page_text=page_text.replace('\u', ' ')
+        for i in range(0,10):
+            page_text = re.sub(pattern, r'\1 \2', page_text)
+            page_text = re.sub(pattern2, r'\1 \2', page_text)
+            page_text = re.sub(pattern3, r'\1 \2', page_text)
+            page_text = re.sub(pattern4, r'\1 \2', page_text)
+        page_text=page_text.replace('N F L', 'NFL')
+        page_text=page_text.replace('M L B', 'MLB')
+        page_text=page_text.replace('N B A', 'NBA')
+        page_text=page_text.replace('N H L', 'NHL')
+        page_text=page_text.replace('P M', 'pm')
+        page_text=page_text.replace('A M', 'am')
+        page_text=page_text.replace('\n', ' ')
+        #page_text=page_text.split("Featured Snippets")[0]
+        page_text=page_text.split("Verbatim")[1]
+        #page_text=page_text.split(".")[0]
+        #page_text=page_text.split("?")[0]
+        page_text=page_text.split("All times are in Eastern Time")[0]
+        #page_text=page_text.split("-")[0]
+        try:
+            if 'def' in url:
+                page_text=page_text.split("/")[2]
+            pass
+        except:
+            pass
+        page_text=page_text.split("People also ask")[0]
+        page_text=page_text.split("Others want to know")[0]
+        page_text=page_text.split("More questions")[0]
+        page_text=page_text.replace(' F ', ' fahrenheit ')
+        page_text=page_text.replace(' Q ', ' Quarter ')
+        page_text=page_text.replace(' Final,', '')
+        page_text = re.split(r'\.(?=[A-Z])', page_text)[0]
+        if 'eather' in url:
+            page_text = re.split(r'(?<=[a-z])\s(?=[A-Z])', page_text)[0]
+        try:
+            page_text=page_text.split("/")[1]
+        except IndexError:
+            pass
+        if '...' in page_text or 'www.' in page_text or '.com' in page_text or '.org' in page_text or '.gov' in page_text or '.edu' in page_text or '.io' in page_text:
+            speak('opening in browser')
+            page_text=' '
+            os.system('chromium-browser "'+url+'"')
+        return page_text
+    else:
+        return f"Error: Unable to retrieve content. Status code {response.status_code}"
 # Find weather
 def weather():
     global weather
@@ -445,11 +531,12 @@ def question(qstn):
                 audio=r.listen(source)
                 print('')
                 saidgtxt=r.recognize_google(audio)
-                saidgtxt=saidgtxt.replace(' ', '+')
-                saidgtxt=saidgtxt.replace("'", '+')
+                #saidgtxt=saidgtxt.replace(' ', '+')
+                #saidgtxt=saidgtxt.replace("'", '+')
         except:
             saidgtxt=" "
-        os.system('xdg-open https://www.google.com/search?q='+saidgtxt+' &')
+        #os.system('xdg-open https://www.google.com/search?q='+saidgtxt+' &')
+        screen(google_search(saidgtxt))
         moodometer=[1,2,3,4,5,6]
     elif 'I will' in qstn or 'definately' in qstn:
         screen('that is good')
@@ -610,9 +697,10 @@ def question(qstn):
         except:
             saidgtxt="No"
         if 'yes' in saidgtxt or 'yeah' in saidgtxt or 'sure' in saidgtxt:
-            qstn=qstn.replace(' ', '+')
-            qstn=qstn.replace("'", '+')
-            os.system('xdg-open https://www.google.com/search?q='+qstn+' &')
+            #qstn=qstn.replace(' ', '+')
+            #qstn=qstn.replace("'", '+')
+            #os.system('xdg-open https://www.google.com/search?q='+qstn+' &')
+            screen(google_search(qstn))
         moodometer=[1,2,3,4,6]
     elif qstn == 'no' or 'no ' in qstn:
         screen('ok')
