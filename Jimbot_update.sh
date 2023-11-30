0a32c331edd40fcdd5134308040f61e36e3db008
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-11-29 21:11:55 -0500

    Add files via upload

--- Jimbot_update.sh
+++ Jimbot_update.sh
@@ -5,6 +5,7 @@ mv ~/Jimbot/Welcome/info.py ~
 mv ~/Jimbot/Python/new_com.py ~
 mv ~/Jimbot/Python/skills ~
 mv ~/Jimbot/Hotword/Jimbot.ppn ~
+mv ~/Jimbot/Hotword/hinfo.py ~
 mv ~/Jimbot/Dependencies ~/Jimbot-Dependencies
 rm -r ~/Jimbot
 git clone https://github.com/Mrpi314tech/Jimbot
@@ -18,6 +19,7 @@ mv ~/new_words.py ~/Jimbot/Python
 mv ~/info.py ~/Jimbot/Welcome
 mv ~/new_com.py ~/Jimbot/Python
 mv ~/Jimbot.ppn ~/Jimbot/Hotword
+mv ~/hinfo.py ~/Jimbot/Hotword
 mv ~/Jimbot_update.sh ~/delete_this_file-Jimbot
 mv ~/Jimbot-Dependencies ~/Jimbot/Dependencies
 echo "done"
