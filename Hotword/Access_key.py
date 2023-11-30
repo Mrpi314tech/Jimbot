f2e1dd894e8d27d040e55b1f9fcd299e8acebcf5
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-11-29 21:07:02 -0500

    Add files via upload

--- Hotword/Access_key.py
+++ Hotword/Access_key.py
@@ -1,7 +1,7 @@
 import os
 print(os.path.expanduser('~'))
 file_location=os.path.expanduser('~')
-pvp_passkey = input('What is your Picovoice Access Key?')
+pvp_passkey = input('Access Key from https://picovoice.ai ')
 file2 = open(file_location+"/Jimbot/Hotword/info.py", "w")
 file2.write('pvp_passkey="'+pvp_passkey+'"')
 file2.close()
