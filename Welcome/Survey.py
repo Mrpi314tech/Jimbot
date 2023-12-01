2dcb5603d59e96537e31552040b2d6be9fa3415d
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-12-01 09:54:04 -0500

    Update Survey.py

--- Welcome/Survey.py
+++ Welcome/Survey.py
@@ -2,7 +2,6 @@ import os
 print(os.path.expanduser('~'))
 file_location=os.path.expanduser('~')
 your_name = input('what is your name? ')
-pvp_passkey = input('What is your Picovoice Access Key?')
 file1 = open(file_location+"/Jimbot/Welcome/info.py", "w") 
 file1.write('your_name="'+your_name+'"')
 file1.write('\npvp_passkey="'+pvp_passkey+'"')
